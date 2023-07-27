ef5b5dd76c9c985d28bfb1ef362046a02d435d72
andrew <andrewsmith.independent@gmail.com>
2023-07-26 20:02:43 -0500

    login

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "922c21d9bbdecb96"; }
+/******/ 	__webpack_require__.h = function() { return "0ae1e12e1d381f69"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
@@ -82,6 +82,11 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 		};
 /******/ 	};
 /******/ }
+/******/ 
+/******/ /* webpack/runtime/nonce */
+/******/ !function() {
+/******/ 	__webpack_require__.nc = undefined;
+/******/ }();
 /******/ 
 /******/ }
 );
--- pages/index.js
+++ pages/index.js
@@ -4,31 +4,39 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>;
-}
+export default function HomePage(loggedInUser, loggedoutUser) {
+  loggedInUser = true;
+  loggedoutUser = false;
 
-export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
+  if (loggedoutUser==true) {
+    console.log('logged out user');
+    return (
+          <Container>
+            <h1>hi</h1>
+          </Container>)
   }
+  else if (loggedInUser==true){
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+    const [likes, setLikes] = useState(0);
 
-  return (
-    <Container>
-      <Card style={{ width: '18rem' }}>
-      <Card.Title>Develop. Previews. Ship. 🚀</Card.Title>
-          <Card.Body>
-            <ListGroup>
-                {names.map((name) => (
-                  <ListGroup.Item key={name}>{name}</ListGroup.Item>
-                ))}
-            </ListGroup>
-          </Card.Body>
-        <Button variant ="primary" onClick={handleClick}>Like ({likes})</Button>
-      </Card>  
-    </Container>  
-  );
-}
+    function handleClick() {
+      setLikes(likes + 1);
+    }
+
+    return (
+      <Container>
+        <Card style={{ width: '18rem' }}>
+        <Card.Title>Develop. Previews. Ship. 🚀</Card.Title>
+            <Card.Body>
+              <ListGroup>
+                  {names.map((name) => (
+                    <ListGroup.Item key={name}>{name}</ListGroup.Item>
+                  ))}
+              </ListGroup>
+            </Card.Body>
+          <Button variant ="primary" onClick={handleClick}>Like ({likes})</Button>
+        </Card>  
+      </Container>  
+    );
+  };
+};
